838e2dea0cd201a4cf4e3745827940792b4dc530
Ryan Faircloth <ryan@dss-i.com>
2022-10-10 15:29:04 -0400

    fix: missing path

--- .github/workflows/scripts/prepareHelm.sh
+++ .github/workflows/scripts/prepareHelm.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #
+helm repo add openebs https://openebs.github.io/charts
 yq -i ".version = \"${1}\" | .appVersion style=\"double\" | .appVersion = \"${1}\"" chart/Chart.yaml
-helm dependency build
+helm dependency build chart
 helm package chart
