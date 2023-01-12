4dc9a2471187ac7c3c53c5d71633a531aa80619d
Ryan Faircloth <ryan@dss-i.com>
2023-01-11 19:35:42 -0500

    feat!: not dependent on openebs

--- .github/workflows/scripts/prepareHelm.sh
+++ .github/workflows/scripts/prepareHelm.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 #
-helm repo add openebs https://openebs.github.io/charts
 yq -i ".version = \"${1}\" | .appVersion style=\"double\" | .appVersion = \"${1}\"" chart/Chart.yaml
-helm dependency build chart
 helm package chart
